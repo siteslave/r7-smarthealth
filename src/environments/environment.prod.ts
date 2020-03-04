35786607f4dcd62aa590731768d33d06422f955d
Satit Riapit <siteslave@msn.com>
2020-03-04 14:49:49 +0700

    chage api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   secureCookie: true,
   smartHealthGetTokenUrl: 'https://smarthealth.service.moph.go.th/phps/public/api/v3/gettoken',
   smartHealthImmigrationUrl: 'https://smarthealth.service.moph.go.th/phps/api/immigration/v1',
-  nhsoUrl: 'https://r7app.moph.go.th/nhso'
+  nhsoUrl: 'https://r7app.moph.go.th/api/nhso'
 };
